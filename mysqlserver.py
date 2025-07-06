b87f6fbcddfca3cfa36e1d1f22c2eb892d544b57
MaDhlomoCodes <prescillamkhabela@gmail.com>
2025-07-07 00:58:17 +0200

    SQL connector fixed.

--- mysqlserver.py
+++ mysqlserver.py
@@ -5,8 +5,9 @@ try:
     connection = mysql.connector.connect(
         host='localhost',
         user='root',
-        password=''  # Add your MySQL root password if you have one
+        password='your_password'  # Replace with your real password
     )
+
     if connection.is_connected():
         cursor = connection.cursor()
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
@@ -16,8 +17,7 @@ except Error as e:
     print(f"Error: {e}")
 
 finally:
-    if 'cursor' in locals():
+    if 'connection' in locals() and connection.is_connected():
         cursor.close()
-    if connection.is_connected():
         connection.close()
-print("MySQL connection is closed")
+        print("MySQL connection is closed.")
